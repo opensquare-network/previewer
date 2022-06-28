7b431ec78c5890b999a2c09ad352a1c610896e6e
2nthony <ant.hu@outlook.com>
2022-06-28 15:04:38 +0800

    docs: init demo

---
+++ demo/App.tsx
@@ -0,0 +1,39 @@
+import React from "react";
+import { HtmlPreviewer } from "../src/HtmlPreviewer";
+import { MarkdownPreviewer } from "../src/MarkdownPreviewer";
+import { dynamicIdentity } from "../src/plugins/dynamicIdentity";
+import "./index.css";
+const md = `
+## heading
+
+[Google](https://google.com)
+
+Render as React Component with random number: [github](https://github.com "github")
+`;
+
+const html = `
+<h2>heading</h2>
+
+<p><a href="https://google.com">Google</a></p>
+
+<p>Render as React Component with random number: <span data-identity-container /></p>
+</div>
+`;
+
+function App() {
+  return (
+    <div className="App">
+      <div>
+        <h2>html previewer</h2>
+        <HtmlPreviewer content={html} plugins={[dynamicIdentity]} />
+      </div>
+
+      <div>
+        <h2>markdown previewer</h2>
+        <MarkdownPreviewer content={md} plugins={[dynamicIdentity]} />
+      </div>
+    </div>
+  );
+}
+
+export default App;
