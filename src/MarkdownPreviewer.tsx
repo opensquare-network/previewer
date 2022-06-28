1427d2bf9ebb0faba30e1df23c00ea9c31a9c89a
2nthony <ant.hu@outlook.com>
2022-06-28 18:17:53 +0800

    feat: markdown previewer pass rest props to html previewer

--- src/MarkdownPreviewer.tsx
+++ src/MarkdownPreviewer.tsx
@@ -5,7 +5,12 @@ import { applyPlugins } from "./shared";
 import { PreviewerProps } from "./types";
 
 export function MarkdownPreviewer(props: PreviewerProps) {
-  const { plugins = [], content = "" } = props;
+  const {
+    plugins = [],
+    content = "",
+    className = "markdown-body",
+    ...restProps
+  } = props;
 
   const renderer = new Renderer();
 
@@ -19,6 +24,11 @@ export function MarkdownPreviewer(props: PreviewerProps) {
   const html = marked.parse(content, markedOptions);
 
   return (
-    <HtmlPreviewer className="markdown-body" content={html} plugins={plugins} />
+    <HtmlPreviewer
+      className={className}
+      content={html}
+      plugins={plugins}
+      {...restProps}
+    />
   );
 }
