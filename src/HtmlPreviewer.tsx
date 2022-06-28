916678e0a46683062f6ce68dace1aa13113823e4
2nthony <ant.hu@outlook.com>
2022-06-28 18:17:18 +0800

    feat: html previewer use sanitize html plugin

--- src/HtmlPreviewer.tsx
+++ src/HtmlPreviewer.tsx
@@ -3,19 +3,34 @@ import { Html } from "./components/Html";
 import type { HtmlProps, PreviewerProps } from "./types";
 import { css } from "styled-components";
 import { applyPlugins } from "./shared";
+import { sanitizeHtmlPlugin } from "./plugins";
+import sanitizeHtml from "sanitize-html";
 
 export function HtmlPreviewer(props: PreviewerProps) {
-  const { plugins = [], content = "", className = "html-body" } = props;
+  const {
+    plugins = [],
+    content = "",
+    className = "html-body",
+    allowedTags = sanitizeHtml.defaults.allowedTags.concat([
+      "img",
+      "iframe",
+      "br",
+      "ins",
+      "del",
+    ]),
+  } = props;
 
-  const ref = useRef<HTMLDivElement>(null);
+  const resolvePlugins = [sanitizeHtmlPlugin(allowedTags), ...plugins];
 
   const extraCss: HtmlProps["extraCss"] = [];
-  applyPlugins(plugins, "collectCss", css, (str) => extraCss.push(str));
+  applyPlugins(resolvePlugins, "collectCss", css, (str) => extraCss.push(str));
 
-  const [html] = useState(content);
+  const ref = useRef<HTMLDivElement>(null);
+  const [html, setHtml] = useState(content);
 
   useEffect(() => {
-    applyPlugins(plugins, "onRenderedHtml", ref.current);
+    applyPlugins(resolvePlugins, "processHtml", html, setHtml);
+    applyPlugins(resolvePlugins, "onRenderedHtml", ref.current);
   }, [html]);
 
   return (
