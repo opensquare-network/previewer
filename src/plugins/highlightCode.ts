efd99a52363f66c32c8f1742fe4fa18de859e8c6
2nthony <ant.hu@outlook.com>
2022-07-01 17:19:10 +0800

    feat(plugin): highlight code

---
+++ src/plugins/highlightCode.ts
@@ -0,0 +1,22 @@
+import { Plugin } from "../types";
+import Prism from "prismjs";
+
+/**
+ * @description Install `Prismjs`
+ */
+export function highLightPlugin(): Plugin {
+  return {
+    name: "highlight-code",
+
+    markedOptions(options) {
+      options.highlight = function (code, lang) {
+        if (!lang) {
+          return code;
+        }
+
+        const grammer = Prism.languages[lang] ?? Prism.languages.markup;
+        return Prism.highlight(code, grammer, lang || "markup");
+      };
+    },
+  };
+}
