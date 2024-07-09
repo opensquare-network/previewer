f8f82be8a012fec75855d1ec5b58eba63376ce12
2nthony <ant.hu@outlook.com>
2024-07-09 14:12:10 +0800

    migrate highlight code to marked extension

--- src/MarkdownPreviewer.tsx
+++ src/MarkdownPreviewer.tsx
@@ -1,33 +1,37 @@
-import { marked, Renderer } from "marked";
+import { marked, MarkedOptions, Renderer } from "marked";
 import { useEffect, useState } from "react";
 
 import { HtmlPreviewer } from "./HtmlPreviewer";
-import { highLightPlugin } from "./plugins";
 import { applyPlugins } from "./shared";
 import { PreviewerProps } from "./types";
+import { highlightCodeExtension } from "./extensions/highlightCode";
+
+marked.use(highlightCodeExtension());
 
 export function MarkdownPreviewer(props: PreviewerProps) {
   const { plugins = [], content = "", className = "", ...restProps } = props;
 
-  const resolvePlugins = [...plugins, highLightPlugin()];
-
   const renderer = new Renderer();
 
-  const markedOptions: marked.MarkedOptions = {
+  const markedOptions: MarkedOptions = {
     breaks: true,
     renderer,
   };
 
-  applyPlugins(resolvePlugins, "markedOptions", markedOptions);
+  applyPlugins(plugins, "markedOptions", markedOptions);
 
-  const [html, setHtml] = useState(marked.parse(content, markedOptions));
-  useEffect(() => setHtml(marked.parse(content, markedOptions)), [content]);
+  const [html, setHtml] = useState(
+    marked.parse(content, markedOptions) as string,
+  );
+  useEffect(() => {
+    setHtml(marked.parse(content, markedOptions) as string);
+  }, [content]);
 
   return (
     <HtmlPreviewer
       className={className}
       content={html}
-      plugins={resolvePlugins}
+      plugins={plugins}
       {...restProps}
     />
   );
