c00eda38a0a6cef96508020e847cb01d94d82e97
2nthony <ant.hu@outlook.com>
2024-07-09 14:14:44 +0800

    fix: highlight code extension (#74)
    * sync marked version to subsquare
    
    * fix types
    
    * migrate highlight code plugin to marked extension

---
+++ src/extensions/highlightCode.ts
@@ -0,0 +1,21 @@
+import { markedHighlight } from "marked-highlight";
+import Prism from "prismjs";
+import "prismjs/components/prism-markup.js";
+import "prismjs/components/prism-javascript.js";
+import "prismjs/components/prism-jsx.js";
+import "prismjs/components/prism-typescript.js";
+import "prismjs/components/prism-tsx.js";
+import "prismjs/components/prism-rust.js";
+
+export function highlightCodeExtension() {
+  return markedHighlight({
+    highlight(code, lang) {
+      if (!lang) {
+        return code;
+      }
+
+      const grammer = Prism.languages[lang] ?? Prism.languages.markup;
+      return Prism.highlight(code, grammer, lang || "markup");
+    },
+  });
+}
